8fb4dc3fe33384c5d73137d6cc721c14cc5e0148
Alejandro <alex.bautista.ramos.90@gmail.com>
2018-12-26 09:31:08 -0700

    Added some fixes for the default.conf file, so React can throw results

--- complex-react-app/server/index.js
+++ complex-react-app/server/index.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 
 
 //Retrieve all values from the postgres database
-app.get('/values/all',async(req, res) => {
+app.get('/values/all', async(req, res) => {
     const values = await pgClient.query('SELECT * FROM values');
     res.send(values.rows);
 });

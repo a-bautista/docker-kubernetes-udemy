0a4234414a6bbdea00419ae9417da1e632bd66af
Alejandro <alex.bautista.ramos.90@gmail.com>
2019-01-02 16:49:06 -0700

    Added the fix for the visits app. Had to comment the process.exit(0).

--- previous-to-docker-react/visits/index.js
+++ previous-to-docker-react/visits/index.js
@@ -12,7 +12,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-    process.exit(0);	
+    //process.exit(0);	
     client.get('visits', (err, visits) => {
        res.send('Number of visits is ' + visits);
        client.set('visits', parseInt(visits) + 1);
